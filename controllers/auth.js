168af7331e89a00c7965ddfdb3c17eea81616f7c
alina513 <130853290+alina513@users.noreply.github.com>
2024-03-10 20:56:18 +0200

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,11 +16,10 @@ if (user) {
     throw HttpError(409, "Email in use")
 };
 // const hashpassword = await bcrypt.hash(password, 10);
+const newUser = await User.create({ ...req.body });
 const payload = {
-    // id: user._id,  
-    id: 4556677788899999
+    id: newUser._id,  
 };
-const newUser = await User.create({...req.body});
 const token = jwt.sign(payload, SECRET_KEY, {expiresIn:"23h"});
 res.status(201).json({token,
     user: {
