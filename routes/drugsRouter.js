3cbeef3b5fc0d8536eb6a39de961f173beea85c3
alina513 <130853290+alina513@users.noreply.github.com>
2024-03-10 23:32:25 +0200

    Update drugsRouter.js

--- routes/drugsRouter.js
+++ routes/drugsRouter.js
@@ -1,7 +1,11 @@
 const express = require("express");
+import {schemas} from "../models/drug.js"
 
 const ctrl = require("../controllers/drugsControllers.js");
 const drugsRouter = express.Router();
-
+drugsRouter.get(
+    "/",
+    validateBody(schemas.createDrugSchema),
+    ctrl.getAllDrugs)
 
 module.exports = drugsRouter;
